f59512ee79ef949797ffe1a196f96fcadd2b06d9
Yoan Estrada <yoanestrada01@gmail.com>
2022-05-03 05:08:34 -0500

    ADD: feature list article card photos implements and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { Formik, Form, Field } from 'formik'
 import './header.css'
+import './content.css'
+import './article.css'
 import { useState } from 'react'
 
 const App = () => {
   const [photos, setPhotos] = useState([])
+
+  const open = (urlPhoto) => {
+    window.open(urlPhoto)
+  }
+
   console.log('Photos', photos)
+
   return (
     <div>
       <header>
@@ -29,6 +37,16 @@ const App = () => {
           </Form>
         </Formik>
       </header>
+      <section className='container'>
+        <div className='center'>
+          {photos.map((photo) => (
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} alt='' />
+              <p>{[photo.description, photo.alt_description].join('-')}</p>
+            </article>
+          ))}
+        </div>
+      </section>
     </div>
   )
 }
